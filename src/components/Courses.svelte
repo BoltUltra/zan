75a7d9f1c2db321176e5ed834aad1a797f25d158
BoltUltra <mayowa.u.sunusi@gmail.com>
2022-12-16 20:58:22 +0100

    ...

--- src/components/Courses.svelte
+++ src/components/Courses.svelte
@@ -1,15 +1,19 @@
 <script></script>
 
 <div class="lg:px-40 px-5 md:space-y-10 space-y-2 pb-20">
-	<div class="flex lg:flex-row flex-col lg:items-end">
+	<div class="flex md:flex-row flex-col md:items-end">
 		<div class="">
-			<img src="/zan.png" alt="" class="w-20 lg:w-full" />
+			<img src="/zan.png" alt="" class="w-20 md:w-full" />
 		</div>
-		<div class="relative lg:mt-32">
+		<div class="relative md:mt-32">
 			<img src="/zan1.png" alt="" class="rounded-xl" />
-			<img src="/zan2.png" alt="" class="absolute bottom-0 right-0 md:w-80 w-32 lg:block hidden" />
+			<img
+				src="/zan2.png"
+				alt=""
+				class="absolute bottom-0 right-0 lg:w-80 md:w-64 w-32 md:block hidden"
+			/>
 			<p
-				class="absolute text-white flex flex-col lg:top-10 top-3 lg:left-8 left-3 md:pr-72 md:text-lg text-xs"
+				class="absolute text-white flex flex-col lg:top-10 md:top-5 top-3 lg:left-8 left-3 lg:pr-72 md:pr-64 lg:text-lg md:text-base text-xs"
 			>
 				<span>
 					Explore the fastest growing affiliate network and start promoting different categories of
@@ -22,15 +26,15 @@
 			</p>
 		</div>
 	</div>
-	<div class="flex lg:flex-row flex-col items-end">
+	<div class="flex md:flex-row flex-col items-end">
 		<div class="">
-			<img src="/hisu.png" alt="" class="w-20 lg:w-full" />
+			<img src="/hisu.png" alt="" class="w-20 md:w-full" />
 		</div>
-		<div class="relative lg:mt-32">
+		<div class="relative md:mt-32">
 			<img src="/hisu2.png" alt="" class="rounded-xl" />
-			<img src="/hisu1.png" alt="" class="absolute bottom-0 right-0 md:w-96 w-32 lg:block hidden" />
+			<img src="/hisu1.png" alt="" class="absolute bottom-0 right-0 md:w-96 w-32 md:block hidden" />
 			<p
-				class="absolute text-white flex flex-col lg:top-10 top-5 lg:left-8 left-3 lg:pr-72 md:text-lg text-xs"
+				class="absolute text-white flex flex-col lg:top-10 top-5 lg:left-8 left-3 lg:pr-72 md:pr-64 lg:text-lg md:text-base text-xs"
 			>
 				<span>
 					Explore over 10 high income skills and 4 ways to monetize them. This is the only
@@ -40,15 +44,15 @@
 			</p>
 		</div>
 	</div>
-	<div class="flex lg:flex-row flex-col lg:items-end">
+	<div class="flex md:flex-row flex-col md:items-end">
 		<div class="">
-			<img src="/lbs.png" alt="" class="w-20 lg:w-full" />
+			<img src="/lbs.png" alt="" class="w-20 md:w-full" />
 		</div>
-		<div class="relative lg:mt-32">
+		<div class="relative md:mt-32">
 			<img src="/lbs1.png" alt="" class="rounded-xl" />
-			<img src="/lbs2.png" alt="" class="absolute bottom-0 right-0 md:w-96 w-32 md:block hidden" />
+			<img src="/lbs2.png" alt="" class="absolute bottom-0 right-0 md:w-72 w-32 md:block hidden" />
 			<p
-				class="absolute text-white flex flex-col md:top-10 top-5 md:left-8 left-5 lg:pr-80 lg:text-lg text-xs"
+				class="absolute text-white flex flex-col lg:top-10 md:top-7 top-5 lg:left-8 md:left-5 left-3 lg:pr-80 md:pr-64 lg:text-lg md:text-base text-xs"
 			>
 				<span>
 					Starting a business is one thing, growing and scaling the business to massive
@@ -59,15 +63,19 @@
 			</p>
 		</div>
 	</div>
-	<div class="flex lg:flex-row flex-col items-end">
+	<div class="flex md:flex-row flex-col items-end">
 		<div class="">
-			<img src="/web3.png" alt="" class="w-20 lg:w-full" />
+			<img src="/web3.png" alt="" class="w-20 md:w-full" />
 		</div>
-		<div class="relative lg:mt-32">
+		<div class="relative md:mt-32">
 			<img src="/web32.png" alt="" class="rounded-xl" />
-			<img src="/web31.png" alt="" class="absolute bottom-0 right-0 md:w-96 w-32 lg:block hidden" />
+			<img
+				src="/web31.png"
+				alt=""
+				class="absolute bottom-0 right-0 lg:w-96 md:w-80 w-32 md:block hidden"
+			/>
 			<p
-				class="absolute text-white flex flex-col md:top-10 top-5 md:left-8 left-5 lg:pr-72 md:text-lg text-xs"
+				class="absolute text-white flex flex-col lg:top-10 md:top-5 top-5 lg:left-8 md:left-5 left-3 lg:pr-72 md:pr-64 md:text-lg text-xs"
 			>
 				<span>
 					We are in the future. <br />
